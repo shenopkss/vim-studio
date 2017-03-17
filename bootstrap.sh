86af457865b46790367ee2e7df4004d0f8f173c5
shenxingshan <xingshanshen@pptv.com>
2017-03-17 13:32:20 +0800

    v1.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
-mv "$HOME/.vim" "$HOME/.vim.bak"
+mkdir autoload
+curl -fLo ./autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s "$PWD/vimrc" "$HOME/.vimrc"
+ln -s "$PWD/vimrc.bundles" "$HOME/.vimrc.bundles"
 ln -s $PWD "$HOME/.vim"
 
-mkdir colors
-wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ./colors/molokai.vim
-wget https://raw.githubusercontent.com/dracula/vim/926dfbab01128322f6326bdd10de2856b1fa3232/colors/dracula.vim -O ./colors/dracula.vim
-
-git clone https://github.com/rking/ag.vim ./bundle/ag 
-git clone https://github.com/kien/ctrlp.vim.git ./bundle/ctrlp.vim
+vim +PlugInstall +q
